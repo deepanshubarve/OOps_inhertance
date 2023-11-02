2cf7abeb99ed3526675e59789920c6d45e05e484
Arjit Gautam <111347114+deepanshubarve@users.noreply.github.com>
2023-11-02 23:26:35 +0530

    Fix second small

--- src/array/DuplicateArray.java
+++ src/array/DuplicateArray.java
@@ -1,12 +1,15 @@
 package array;
 
+import java.util.Arrays;
+
 public class DuplicateArray {
     public static void main(String[] args) {
-        int[]arr = {1,2,3,1,2};
-
-    }
-
-    public static void dupli(){
+        int[] arr = {1, 2, 3, 1, 2};
+        System.out.println(Arrays.toString(arr));
 
     }
 }
+
+
+
+
