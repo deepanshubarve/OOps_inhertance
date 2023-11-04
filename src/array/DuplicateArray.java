da4d52dea07c3e585a6579b1c68543c448cd430f
Arjit Gautam <111347114+deepanshubarve@users.noreply.github.com>
2023-11-04 23:15:57 +0530

    Fix second small

--- src/array/DuplicateArray.java
+++ src/array/DuplicateArray.java
@@ -5,7 +5,12 @@ import java.util.Arrays;
 public class DuplicateArray {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1, 2};
-        System.out.println(Arrays.toString(arr));
+
+        for(int i =1;i<arr.length;i++){
+            if(arr[i-1] < arr[i]){
+                System.out.println(arr[i]);
+            }
+        }
 
     }
 }
